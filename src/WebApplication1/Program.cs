b08b7d00c666c4e020050e6b54ad5bbae053eb05
SeungYongShim <syshim@me.com>
2023-08-12 10:47:07 +0900

    .

--- src/WebApplication1/Program.cs
+++ src/WebApplication1/Program.cs
@@ -1,8 +1,27 @@
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOptions();
+builder.Services.AddOptions<App1DbOption>().BindConfiguration("App1");
+builder.Services.AddOptions<App2DbOption>().BindConfiguration("App2");
 
 var app = builder.Build();
 
+var ret1 = app.Services.GetRequiredService<IOptions<DbOption>>();
+var ret2 = app.Services.GetRequiredService<IEnumerable<IOptions<DbOption>>>();
+var ret3 = app.Services.GetRequiredService<IOptions<App1DbOption>>();
+var ret4 = app.Services.GetRequiredService<IOptions<App2DbOption>>();
+
+
 app.Run();
 
+
+public record DbOption
+{
+    public string DbConnRO { get; init; }
+    public string DbConnRW { get; init; }
+}
+
+public record App1DbOption : DbOption;
+public record App2DbOption : DbOption;
--- src/WebApplication1/SimpleConfig.cs
+++ src/WebApplication1/SimpleConfig.cs
@@ -2,22 +2,3 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace WebApplication1;
 
-
-public interface IConfigFactory<T>
-{
-    T Create<TAppName>(TAppName appName) where TAppName : struct, Enum => Create(Enum.GetName(appName)!);
-    T Create(string appName);
-}
-
-public class ConfigFactory<T> : IConfigFactory<T>
-{
-    public T Create(string appName) => 
-}
-
-public static class SimpleConfigExtenions
-{
-    public static IServiceCollection AddConfigFactory(this IServiceCollection services)
-    {
-        services.TryAddSingleton(typeof(IConfigFactory<>, ConfigFactory<>));
-    }
-}
